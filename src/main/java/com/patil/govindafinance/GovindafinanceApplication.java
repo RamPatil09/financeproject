6913a2644acb7cac86f1323db2439b70ed97f35f
RamPatil09 <ramangoudpatil.xworkz@gmail.com>
2025-05-08 23:49:37 +0530

    Uploading the project

---
+++ src/main/java/com/patil/govindafinance/GovindafinanceApplication.java
@@ -0,0 +1,13 @@
+package com.patil.govindafinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GovindafinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GovindafinanceApplication.class, args);
+	}
+
+}
